b89824d650b2425877ee23282aa250251c45afb7
Henrik Oldenborg <henrik.oldenborg@lego.com>
2020-09-16 16:40:21 +0200

    Refactor home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,35 @@
 <template lang="pug">
-  .home Home page
+  v-container(fluid fill-height)
+    v-row(:justify="'center'" :align="'center'")
+      v-col(xs12 sm8 md4 lg3)
+        h1.display Login
+        v-form.mb-5(ref="form" lazy-validation)
+          v-text-field(v-model="username" label="E-mail" name="username" :rules="[rules.required]")
+          v-text-field(label="Password" name="password" v-model="password" :append-icon="show ? 'visibility' : 'visibility_off'" :type="show ? 'text' : 'password'" @click:append="show = !show" :rules="[rules.required]")
+          v-btn(name="submit_login" color="primary" @click="login" block) Login
+        div: router-link(to="/password/reset") Forgot your password?
+        div: router-link(to="/register") Don't have an account?
+        div: router-link(to="/") Back to frontpage
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data: () => {
+    return {
+      show: false,
+      username: "",
+      password: "",
+      rules: {
+        required: v => !!v || "This field is is required"
+      }
+    };
+  },
+  methods: {
+    async login() {
+      this.$refs.form.validate();
+      // TODO: Send login request and redirect if successfull
+    }
+  }
 };
 </script>
