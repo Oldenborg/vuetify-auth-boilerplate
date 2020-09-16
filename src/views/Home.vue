1a51ca1ac0c069303243340fd9f884b8195e3741
Henrik Oldenborg <henrik.oldenborg@lego.com>
2020-09-16 18:08:54 +0200

    Expand login view validation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,11 @@
     v-row(:justify="'center'" :align="'center'")
       v-col(xs12 sm8 md4 lg3)
         h1.display Login
-        v-form.mb-5(ref="form" lazy-validation)
-          v-text-field(v-model="username" label="E-mail" name="username" :rules="[rules.required]")
+        v-form(ref="form" lazy-validation)
+          v-text-field(v-model="username" label="E-mail" name="username" :rules="[rules.required, rules.validEmail]")
           v-text-field(label="Password" name="password" v-model="password" :append-icon="show ? 'visibility' : 'visibility_off'" :type="show ? 'text' : 'password'" @click:append="show = !show" :rules="[rules.required]")
           v-btn(name="submit_login" color="primary" @click="login" block) Login
+        v-divider.my-4
         div: router-link(to="/password/reset") Forgot your password?
         div: router-link(to="/register") Don't have an account?
 </template>
@@ -20,14 +21,17 @@ export default {
       username: "",
       password: "",
       rules: {
-        required: v => !!v || "This field is is required"
+        required: v => !!v || "This field is is required",
+        validEmail: v => /.+@.+\..+/.test(v) || "E-mail must be valid"
       }
     };
   },
   methods: {
     async login() {
-      this.$refs.form.validate();
-      // TODO: Send login request and redirect if successfull
+      if (!this.$refs.form.validate()) {
+        return;
+      }
+      this.$router.push("/dashboard");
     }
   }
 };
