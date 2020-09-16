9c710a9200c276467a9dc293cdc24cc6d16ffc16
Henrik Oldenborg <henrik.oldenborg@lego.com>
2020-09-16 18:10:20 +0200

    Add check registration email view

---
+++ src/views/CheckRegistrationEmail.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+v-container(fluid fill-height)
+  v-row(:justify="'center'" :align="'center'")
+    v-col(xs12 sm8 md4 lg3).text-center
+      v-icon.mb-4(size="15rem") check_circle_outline
+      h1 Success
+      p To complete the registration we need you to activate your account by confirming your email address. We have sent you an e-email with an activation link.
+      v-divider.my-4
+      div: router-link(to="/login") Login
+      div: router-link(to="/") Back to frontpage
+</template>
