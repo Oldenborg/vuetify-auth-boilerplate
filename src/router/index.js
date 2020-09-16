38a6114e4b0674301c8e8a7937553965e89dd508
Henrik Oldenborg <henrik.oldenborg@lego.com>
2020-09-16 18:08:19 +0200

    Add remaining routes

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,59 @@ const routes = [
     name: "Register",
     component: () =>
       import(/* webpackChunkName: "register" */ "../views/Register.vue")
+  },
+  {
+    path: "/password/reset",
+    name: "reset-password",
+    component: () =>
+      import(
+        /* webpackChunkName: "reset-password" */ "../views/ResetPassword.vue"
+      )
+  },
+  {
+    path: "/register/confirm",
+    name: "register-confirm",
+    component: () =>
+      import(
+        /* webpackChunkName: "register-confirm" */ "../views/RegisterConfirm.vue"
+      )
+  },
+  {
+    path: "/password/new",
+    name: "new-password",
+    component: () =>
+      import(/* webpackChunkName: "new-password" */ "../views/NewPassword.vue")
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    meta: {
+      auth: true
+    },
+    component: () =>
+      import(/* webpackChunkName: "dashboard" */ "../views/Dashboard.vue")
+  },
+  {
+    path: "/check-registration-email",
+    name: "check-registration-email",
+    component: () =>
+      import(
+        /* webpackChunkName: "check-registration-email" */ "../views/CheckRegistrationEmail.vue"
+      )
+  },
+  {
+    path: "/check-reset-password-email",
+    name: "check-reset-password-email",
+    component: () =>
+      import(
+        /* webpackChunkName: "check-reset-password-email" */ "../views/CheckResetPasswordEmail.vue"
+      )
+  },
+  {
+    path: "/terms",
+    name: "terms",
+    component: () =>
+      import(/* webpackChunkName: "terms" */ "../views/Terms.vue")
   }
 ];
 
@@ -26,5 +79,22 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 });
+
+// Authentication check
+//
+// router.beforeEach(async (to, from, next) => {
+//   if (to.matched.some((record) => record.meta.auth)) {
+//     if (!store.getters["Auth/authenticated"]) {
+//       next({
+//         path: "/",
+//         query: { redirect: to.fullPath },
+//       });
+//     } else {
+//       next();
+//     }
+//   } else {
+//     next();
+//   }
+// });
 
 export default router;
